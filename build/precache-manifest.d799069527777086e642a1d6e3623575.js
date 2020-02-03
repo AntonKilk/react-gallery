682d034b30a568a411a0cb7bc7d550bd3610f851
AntonKilk <ant.kilk@gmail.com>
2020-02-03 06:40:36 +0200

    build add

---
+++ build/precache-manifest.d799069527777086e642a1d6e3623575.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7750e8666ff83b46cf5b5069db9527da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b23df84c8f91735da6f",
+    "url": "/static/css/main.28430a44.chunk.css"
+  },
+  {
+    "revision": "0e57c826cc3fea787dfc",
+    "url": "/static/js/2.a519ec23.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.a519ec23.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4b23df84c8f91735da6f",
+    "url": "/static/js/main.cf3b8552.chunk.js"
+  },
+  {
+    "revision": "a140cc0fa6b9f31c5819",
+    "url": "/static/js/runtime-main.a6d53069.js"
+  }
+]);
